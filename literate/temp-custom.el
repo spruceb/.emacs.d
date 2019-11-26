54840b1787d86a273fcf399596bd1103a8c2b75e
Spruce Bondera <sprucebondera@gmail.com>
2019-11-26 13:03:25 -0800

    Split this commit later

--- literate/temp-custom.el
+++ literate/temp-custom.el
@@ -7,11 +7,14 @@
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "926cf2e79abc7465cbe92759d0124c68e9c06d4147cc29217b229fe52f0411bd" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" default)))
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(erc-log-mode t)
  '(helm-descbinds-mode t)
+ '(org-tags-column -60)
  '(package-selected-packages
    (quote
-    (ebnf-mode meta-syntax-faces font-lock-ext intero fish-mode ag blue-spruce projectile-ripgrep lua-mode achievements rust-mode bison-mode go-mode haskell-mode fireplace default-text-scale doom-themes csv-mode rtags helm-descbinds circe ess toc-org validate htmlize paradox company-auctex auctex markdown-mode yasnippet w3m visible-mark use-package undo-tree transpose-frame spaceline spacegray-theme smartparens smart-mode-line-powerline-theme slime reveal-in-finder rainbow-mode pyvenv python-info pylint py-autopep8 persistent-scratch org-pandoc org-bullets multiple-cursors magit llvm-mode julia-mode indent-guide highlight-indentation highlight-indent-guides helm-swoop helm-pydoc helm-projectile helm-flx helm-company helm-ag flycheck find-file-in-project fill-column-indicator emms dtrt-indent debbugs company-jedi column-marker color-theme buffer-move auto-complete aggressive-indent)))
+    (scss-mode tide typescript-mode intero yaml-mode dired-subtree dired helm-org-rifle ace-jump-mode helm-rg gitlab omnisharp omnisharp-emacs csharp-mode company-jedi elpy coin-ticker osx-plist org-brain centered-window-mode unfill togetherly org ebnf-mode meta-syntax-faces font-lock-ext fish-mode ag blue-spruce projectile-ripgrep lua-mode achievements rust-mode bison-mode go-mode haskell-mode fireplace default-text-scale doom-themes csv-mode rtags helm-descbinds circe ess toc-org validate htmlize paradox company-auctex auctex markdown-mode yasnippet w3m visible-mark use-package undo-tree transpose-frame spaceline spacegray-theme slime reveal-in-finder rainbow-mode pyvenv python-info pylint py-autopep8 persistent-scratch org-pandoc org-bullets multiple-cursors magit llvm-mode julia-mode indent-guide highlight-indentation highlight-indent-guides helm-swoop helm-pydoc helm-projectile helm-flx helm-company helm-ag flycheck find-file-in-project fill-column-indicator emms dtrt-indent debbugs column-marker color-theme buffer-move auto-complete aggressive-indent)))
  '(paradox-github-token t)
  '(safe-local-variable-values
    (quote
@@ -23,4 +26,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "#22252c"))))
  '(linum ((t (:background "#22252c" :foreground "light gray" :weight normal)))))
